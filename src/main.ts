5f7f94731f577a36a64d05e673a65bf1046f2138
luecha.kan <luecha.kanm@gmail.com>
2025-03-31 21:44:52 +0700

    verify login

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,14 @@ async function bootstrap() {
   app.enableCors({
     origin: `${process.env.CORS_ORIGIN}`,
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   });
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  
   new SwaggerDoc().setupDocs(app, port);
 
   await app.listen(port);
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,22 +7,22 @@ import { UserAddressService } from './../user-address/user-address.service';
 import { TambonsService } from './../tambons/tambons.service';
 import { LoginDto } from './dto/login.dto';
 import {
-  BadRequestException,
-  Body,
-  ClassSerializerInterceptor,
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-  NotFoundException,
-  Param,
-  Patch,
-  Post,
-  Query,
-  Req,
-  Res,
-  UseGuards,
-  UseInterceptors,
+    BadRequestException,
+    Body,
+    ClassSerializerInterceptor,
+    Controller,
+    Get,
+    HttpException,
+    HttpStatus,
+    NotFoundException,
+    Param,
+    Patch,
+    Post,
+    Query,
+    Req,
+    Res,
+    UseGuards,
+    UseInterceptors,
 } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 import { RegisterDto } from './dto/register.dto';
@@ -38,208 +38,213 @@ import { ApiTags } from '@nestjs/swagger';
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private userService: UserService,
-    private jwtService: JwtService,
-    private authService: AuthService,
-    private tambonsService: TambonsService,
-    private userAddressService: UserAddressService,
-    private roleService: RoleService,
-    private marketService: MarketService,
-    private adminMarketService: AdminMarketService,
-  ) {}
+    constructor(
+        private userService: UserService,
+        private jwtService: JwtService,
+        private authService: AuthService,
+        private tambonsService: TambonsService,
+        private userAddressService: UserAddressService,
+        private roleService: RoleService,
+        private marketService: MarketService,
+        private adminMarketService: AdminMarketService,
+    ) { }
 
-  @Post('register')
-  async register(@Body() body: RegisterDto): Promise<BaseResultData> {
-    try {
-      const {
-        firstname,
-        lastname,
-        phoneNumber,
-        email,
-        dateOfBirth,
-        gender,
-        detailAddress,
-        password,
-        confirmPassword,
-        province,
-        amphoe,
-        district,
-        zipCode,
-        roleCode,
-        market,
-      } = body;
+    @Post('register')
+    async register(@Body() body: RegisterDto): Promise<BaseResultData> {
+        try {
+            const {
+                firstname,
+                lastname,
+                phoneNumber,
+                email,
+                dateOfBirth,
+                gender,
+                detailAddress,
+                password,
+                confirmPassword,
+                province,
+                amphoe,
+                district,
+                zipCode,
+                roleCode,
+                market,
+            } = body;
 
-      if (password !== confirmPassword) {
-        throw new BadRequestException('Passwords do not match!');
-      }
+            if (password !== confirmPassword) {
+                throw new BadRequestException('Passwords do not match!');
+            }
 
-      const hashedPassword = await bcrypt.hash(password, 12);
+            const hashedPassword = await bcrypt.hash(password, 12);
 
-      const tombans = await this.tambonsService.getTambonDetail(
-        district,
-        amphoe,
-        province,
-        zipCode,
-      );
+            const tombans = await this.tambonsService.getTambonDetail(
+                district,
+                amphoe,
+                province,
+                zipCode,
+            );
 
-      const role = await this.roleService.findOne({ where: { roleCode } });
+            const role = await this.roleService.findOne({ where: { roleCode } });
 
-      const user = await this.userService.create({
-        firstname,
-        lastname,
-        phoneNumber,
-        email,
-        dateOfBirth,
-        gender,
-        active: true,
-        password: hashedPassword,
-        createAt: new Date(),
-        updateAt: new Date(),
-        createBy: 'register',
-        role: { id: role.id },
-      });
+            const user = await this.userService.create({
+                firstname,
+                lastname,
+                phoneNumber,
+                email,
+                dateOfBirth,
+                gender,
+                active: true,
+                password: hashedPassword,
+                createAt: new Date(),
+                updateAt: new Date(),
+                createBy: 'register',
+                role: { id: role.id },
+            });
 
-      await this.userAddressService.create({
-        detailAddress,
-        createAt: new Date(),
-        updateAt: new Date(),
-        tambons: tombans,
-        users: user,
-      });
+            await this.userAddressService.create({
+                detailAddress,
+                createAt: new Date(),
+                updateAt: new Date(),
+                tambons: tombans,
+                users: user,
+            });
 
-      if (roleCode === '2') {
-        const createdMarket = await this.marketService.create({
-          ...market,
-          createAt: new Date(),
-          updateAt: new Date(),
-          updateBy: '',
-          createBy: 'register',
-        });
+            if (roleCode === '2') {
+                const createdMarket = await this.marketService.create({
+                    ...market,
+                    createAt: new Date(),
+                    updateAt: new Date(),
+                    updateBy: '',
+                    createBy: 'register',
+                });
 
-        await this.adminMarketService.create({
-          users: user,
-          markets: createdMarket,
-        });
-      }
+                await this.adminMarketService.create({
+                    users: user,
+                    markets: createdMarket,
+                });
+            }
 
-      const result = await this.userService.getUsersWithRelations(user.id);
+            const result = await this.userService.getUsersWithRelations(user.id);
 
-      return new BaseResultData('201', result, '', 'Success');
-    } catch (error) {
-      throw new HttpException(
-        new BaseResultError('400', error.response?.message, error.message),
-        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+            return new BaseResultData('201', result, '', 'Success');
+        } catch (error) {
+            throw new HttpException(
+                new BaseResultError('400', error.response?.message, error.message),
+                error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
     }
-  }
 
-  @Post('login')
-  async login(
-    @Body() body: LoginDto,
-    @Res({ passthrough: true }) response: Response,
-  ): Promise<BaseResultData> {
-    try {
-      const { email, password } = body;
-      const user = await this.userService.findOne({ email }, ['role']);
+    @Post('login')
+    async login(
+        @Body() body: LoginDto,
+        @Res({ passthrough: true }) response: Response,
+    ): Promise<BaseResultData> {
+        try {
+            const { email, password } = body;
+            const user = await this.userService.findOne({ email }, ['role']);
 
-      if (!user) {
-        throw new NotFoundException('User not found');
-      }
+            if (!user) {
+                throw new NotFoundException('User not found');
+            }
 
-      if (!(await bcrypt.compare(password, user.password))) {
-        throw new BadRequestException('Invalid credentials');
-      }
+            if (!(await bcrypt.compare(password, user.password))) {
+                throw new BadRequestException('Invalid credentials');
+            }
 
-      const jwt = await this.jwtService.signAsync({ id: user.id });
+            const jwt = await this.jwtService.signAsync({ id: user.id });
 
-      response.cookie('access_token', jwt, { httpOnly: false, secure: true });
+            // เปลี่ยนการตั้งค่า cookie ให้ทำงานกับ cross-domain
+            response.cookie('access_token', jwt, {
+                httpOnly: false,
+                secure: true,
+                sameSite: 'none', // จำเป็นสำหรับ cross-domain
+            });
 
-      return new BaseResultData('200', user, jwt, 'Success');
-    } catch (error) {
-      throw new HttpException(
-        new BaseResultError('400', error.response?.message, error.message),
-        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+            return new BaseResultData('200', user, jwt, 'Success');
+        } catch (error) {
+            throw new HttpException(
+                new BaseResultError('400', error.response?.message, error.message),
+                error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
     }
-  }
 
-  @UseGuards(AuthGuard())
-  @Get('user-info')
-  async user(@Req() request: Request) {
-    try {
-      const id = await this.authService.userId(request);
+    @UseGuards(AuthGuard())
+    @Get('user-info')
+    async user(@Req() request: Request) {
+        try {
+            const id = await this.authService.userId(request);
 
-      const user = await this.userService.findOne({ id }, ['role']);
+            const user = await this.userService.findOne({ id }, ['role']);
 
-      return new BaseResultData('200', user, '', 'Success');
-    } catch (error) {
-      throw new HttpException(
-        new BaseResultError('400', error.response?.message, error.message),
-        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+            return new BaseResultData('200', user, '', 'Success');
+        } catch (error) {
+            throw new HttpException(
+                new BaseResultError('400', error.response?.message, error.message),
+                error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
     }
-  }
 
-  @UseGuards(AuthGuard())
-  @Post('logout')
-  async logout(@Res({ passthrough: true }) response: Response) {
-    try {
-      response.clearCookie('access_token');
+    @UseGuards(AuthGuard())
+    @Post('logout')
+    async logout(@Res({ passthrough: true }) response: Response) {
+        try {
+            response.clearCookie('access_token');
 
-      return new BaseResultData('200', [], '', 'Success logout');
-    } catch (error) {
-      throw new HttpException(
-        new BaseResultError('400', error.response?.message, error.message),
-        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+            return new BaseResultData('200', [], '', 'Success logout');
+        } catch (error) {
+            throw new HttpException(
+                new BaseResultError('400', error.response?.message, error.message),
+                error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
     }
-  }
 
-  @Get('user-by-email')
-  async findUserByEmail(@Query('email') email: string) {
-    try {
-      const user = await this.userService.findOne({ email }, ['role']);
+    @Get('user-by-email')
+    async findUserByEmail(@Query('email') email: string) {
+        try {
+            const user = await this.userService.findOne({ email }, ['role']);
 
-      if (!user) {
-        throw new NotFoundException('User not found');
-      }
+            if (!user) {
+                throw new NotFoundException('User not found');
+            }
 
-      return new BaseResultData('200', user, '', 'Success');
-    } catch (error) {
-      throw new HttpException(
-        new BaseResultError('400', error.response?.message, error.message),
-        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+            return new BaseResultData('200', user, '', 'Success');
+        } catch (error) {
+            throw new HttpException(
+                new BaseResultError('400', error.response?.message, error.message),
+                error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
     }
-  }
 
-  @Patch('forgot-password/:user_id')
-  async forgotPassword(
-    @Param('user_id') UserId: string,
-    @Body() body: ForgotPasswordDto,
-  ) {
-    try {
-      const { password, confirmPassword } = body;
+    @Patch('forgot-password/:user_id')
+    async forgotPassword(
+        @Param('user_id') UserId: string,
+        @Body() body: ForgotPasswordDto,
+    ) {
+        try {
+            const { password, confirmPassword } = body;
 
-      if (password !== confirmPassword) {
-        throw new BadRequestException('Passwords do not match!');
-      }
+            if (password !== confirmPassword) {
+                throw new BadRequestException('Passwords do not match!');
+            }
 
-      const hashedPassword = await bcrypt.hash(password, 12);
+            const hashedPassword = await bcrypt.hash(password, 12);
 
-      const user = await this.userService.update(UserId, {
-        password: hashedPassword,
-        updateBy: 'forgot-password',
-        updateAt: new Date(),
-      });
+            const user = await this.userService.update(UserId, {
+                password: hashedPassword,
+                updateBy: 'forgot-password',
+                updateAt: new Date(),
+            });
 
-      return new BaseResultData('200', user, '', 'Success');
-    } catch (error) {
-      throw new HttpException(
-        new BaseResultError('400', error.response?.message, error.message),
-        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+            return new BaseResultData('200', user, '', 'Success');
+        } catch (error) {
+            throw new HttpException(
+                new BaseResultError('400', error.response?.message, error.message),
+                error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
     }
-  }
 }
